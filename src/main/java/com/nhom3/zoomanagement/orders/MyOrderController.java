f0ce768852b45d8acfdb4294a1159f0ac1e77489
Your Name <thotv.t1.1821@gmail.com>
2023-09-26 16:38:48 +0700

    update MyOrderController

--- src/main/java/com/nhom3/zoomanagement/orders/MyOrderController.java
+++ src/main/java/com/nhom3/zoomanagement/orders/MyOrderController.java
@@ -9,14 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("orders")
 public class MyOrderController {
-    @Autowired
-    MyOrderRepository myOrderRepository;
-    
-    @PostMapping("create")
-    public MyOrder create() {
-        MyOrder myOrder = new MyOrder();
-        myOrder.setEmail("a@gmail.com");
-        myOrder.setName("tho");
-        return myOrderRepository.save(myOrder);
-    }
 }
