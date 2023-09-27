f502cbcd7c2bda0ce5a1c1c068ca1ead77669291
linhlinh38 <03linhnguyen@gmail.com>
2023-09-27 08:42:39 +0700

    fix-repo

--- src/main/java/com/nhom3/zoomanagement/animals/AnimalsRepository.java
+++ src/main/java/com/nhom3/zoomanagement/animals/AnimalsRepository.java
@@ -1,7 +1,6 @@
 package com.nhom3.zoomanagement.animals;
 
-import com.nhom3.zoomanagement.tests.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimalsRepository extends JpaRepository<Test, String> {
+public interface AnimalsRepository extends JpaRepository<Animal, String> {
 }
--- src/main/java/com/nhom3/zoomanagement/cages/CagesRepository.java
+++ src/main/java/com/nhom3/zoomanagement/cages/CagesRepository.java
@@ -1,7 +1,6 @@
 package com.nhom3.zoomanagement.cages;
 
-import com.nhom3.zoomanagement.tests.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CagesRepository extends JpaRepository<Test, String> {
+public interface CagesRepository extends JpaRepository<Cage, String> {
 }
