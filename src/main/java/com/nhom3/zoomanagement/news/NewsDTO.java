2055e5b964db7d990272a7f16d3b6bfbb81c1eb2
Your Name <thotv.t1.1821@gmail.com>
2023-10-01 23:18:58 +0700

    update dtos

--- src/main/java/com/nhom3/zoomanagement/news/NewsDTO.java
+++ src/main/java/com/nhom3/zoomanagement/news/NewsDTO.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,7 @@ public class NewsDTO {
     private Integer id;
     private String content;
     private String title;
-    private Date createdAt;
+    private LocalDateTime createdAt;
     private AccountDTO creator;
 
     public static NewsDTO fromNews(News news, boolean hasCreator) {
